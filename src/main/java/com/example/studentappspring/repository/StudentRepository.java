ff51a24708be2ca0e731014584eebad137b496cf
Smug <sashasmug@gmail.com>
2022-05-04 14:21:08 +0200

    delete system added

--- src/main/java/com/example/studentappspring/repository/StudentRepository.java
+++ src/main/java/com/example/studentappspring/repository/StudentRepository.java
@@ -2,7 +2,6 @@ package com.example.studentappspring.repository;
 
 import com.example.studentappspring.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
